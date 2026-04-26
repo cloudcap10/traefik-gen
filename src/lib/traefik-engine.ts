57d242cb3c8717c4bbf9d24b4c8718b27c292380
Edgemind <edgemind@talz.net>
2026-04-26 11:54:31 +0000

    feat: establish TraefikGen Standard (traefik-net, web/websecure, dual-router)

--- src/lib/traefik-engine.ts
+++ src/lib/traefik-engine.ts
@@ -1,29 +1,35 @@
 export function injectTraefikLabels(composeObj: any, config: any) {
-  const { appName, domain, port } = config;
+  const { appName, domain, port, resolver = 'cloudflare' } = config;
+  
   const labels = [
     "traefik.enable=true",
-    `traefik.http.routers.${appName}.entrypoints=http`,
+    // HTTP Router & Redirect
+    `traefik.http.routers.${appName}.entrypoints=web`,
     `traefik.http.routers.${appName}.rule=Host(\`${appName}.${domain}\`)`,
-    `traefik.http.middlewares.${appName}-https-redirect.redirectscheme.scheme=https`,
-    `traefik.http.routers.${appName}.middlewares=${appName}-https-redirect`,
-    `traefik.http.routers.${appName}-secure.entrypoints=https`,
+    `traefik.http.middlewares.${appName}-redirect.redirectscheme.scheme=https`,
+    `traefik.http.routers.${appName}.middlewares=${appName}-redirect`,
+    // HTTPS Router
+    `traefik.http.routers.${appName}-secure.entrypoints=websecure`,
     `traefik.http.routers.${appName}-secure.rule=Host(\`${appName}.${domain}\`)`,
     `traefik.http.routers.${appName}-secure.tls=true`,
+    `traefik.http.routers.${appName}-secure.tls.certresolver=${resolver}`,
     `traefik.http.routers.${appName}-secure.service=${appName}`,
+    // Service Port
     `traefik.http.services.${appName}.loadbalancer.server.port=${port}`,
-    "traefik.docker.network=jttnet"
+    "traefik.docker.network=traefik-net"
   ];
   
   const firstServiceName = Object.keys(composeObj.services)[0];
   const service = composeObj.services[firstServiceName];
   
-  service.container_name = `sgjtt${appName}`;
-  service.hostname = `sgjtt${appName}`;
+  // Use clean naming standard
+  service.container_name = appName;
+  service.hostname = appName;
   service.labels = labels;
-  service.networks = ['jttnet'];
+  service.networks = ['traefik-net'];
   
   composeObj.networks = composeObj.networks || {};
-  composeObj.networks['jttnet'] = { external: true };
+  composeObj.networks['traefik-net'] = { external: true };
   
   return composeObj;
 }
