bb4ce78bc6e9c2847c436c6a042d652cdd15ba9f
Edgemind <edgemind@talz.net>
2026-04-26 11:57:00 +0000

    refactor: unify naming (http/https) and implement global redirect standard

--- src/lib/traefik-engine.ts
+++ src/lib/traefik-engine.ts
@@ -1,20 +1,14 @@
 export function injectTraefikLabels(composeObj: any, config: any) {
   const { appName, domain, port, resolver = 'cloudflare' } = config;
   
+  // Since we now have a GLOBAL redirect in traefik.yml, 
+  // apps only need ONE router on the HTTPS entrypoint.
   const labels = [
     "traefik.enable=true",
-    // HTTP Router (Redirect)
-    `traefik.http.routers.${appName}.entrypoints=http`,
+    `traefik.http.routers.${appName}.entrypoints=https`,
     `traefik.http.routers.${appName}.rule=Host(\`${appName}.${domain}\`)`,
-    `traefik.http.middlewares.${appName}-https-redirect.redirectscheme.scheme=https`,
-    `traefik.http.routers.${appName}.middlewares=${appName}-https-redirect`,
-    // HTTPS Router (Secure)
-    `traefik.http.routers.${appName}-secure.entrypoints=https`,
-    `traefik.http.routers.${appName}-secure.rule=Host(\`${appName}.${domain}\`)`,
-    `traefik.http.routers.${appName}-secure.tls=true`,
-    `traefik.http.routers.${appName}-secure.tls.certresolver=${resolver}`,
-    `traefik.http.routers.${appName}-secure.service=${appName}`,
-    // Service Definition
+    `traefik.http.routers.${appName}.tls=true`,
+    `traefik.http.routers.${appName}.tls.certresolver=${resolver}`,
     `traefik.http.services.${appName}.loadbalancer.server.port=${port}`,
     "traefik.docker.network=traefik-net"
   ];
@@ -22,7 +16,6 @@ export function injectTraefikLabels(composeObj: any, config: any) {
   const firstServiceName = Object.keys(composeObj.services)[0];
   const service = composeObj.services[firstServiceName];
   
-  // Apply your Production-Grade Standards
   service.container_name = appName;
   service.hostname = appName;
   service.restart = "unless-stopped";
